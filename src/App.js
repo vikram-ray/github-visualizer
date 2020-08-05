1f0065e805b5e33aa6513083d554ff30bc12cc8f
rayvikram <vik8876@gmail.com>
2020-08-06 00:39:07 +0530

    removed defaults

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Visualizer from './features/visualizer/Visualizer'
+import { toast } from 'react-toastify';
+
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure({
+  autoClose: 2500,
+  position: toast.POSITION.TOP_RIGHT,
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+       <Visualizer />
     </div>
   );
 }
