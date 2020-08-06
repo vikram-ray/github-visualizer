f08c2b104eb2f9b855b8aa5190032dd60b844587
rayvikram <vik8876@gmail.com>
2020-08-06 22:10:53 +0530

    Router addded in appjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+
+
 import Visualizer from './features/visualizer/Visualizer'
 import { toast } from 'react-toastify';
 
@@ -8,12 +11,27 @@ import 'react-toastify/dist/ReactToastify.css';
 toast.configure({
   autoClose: 2500,
   position: toast.POSITION.TOP_RIGHT,
-});
+}); 
+
+const NotFound = () => <h1>The page you are looking does not exist</h1>
+
+function Routes() {
+  return(
+    <Suspense fallback="Loading" >
+      <Switch>
+        <Route exact path={['/', '/:username']} component={Visualizer} />
+        <Route component={NotFound} />
+      </Switch>
+    </Suspense>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-       <Visualizer />
+      <BrowserRouter>
+       <Routes />
+      </BrowserRouter>
     </div>
   );
 }
