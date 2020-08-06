25424cfb004e09a10ef906bca72af5f4f85abb6c
rayvikram <vik8876@gmail.com>
2020-08-06 22:11:23 +0530

    Counter removed

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import visualizerReducer from '../features/visualizer/visualizerSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     visualizer: visualizerReducer,
   },
 });
